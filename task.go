cd968395a2b44cd18fbc13a54c3406195cf35c4b
Denys Chernousov <blackInOwls@gmail.com>
2024-02-16 11:16:20 +0200

    taskv3.go is done

--- task.go
+++ task.go
@@ -31,15 +31,20 @@ func NewCell(value int8) Cell {
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 func main() {
-	initial := getTaskInit()
+
+	initial := getExampleInit()
 	result := SolveMatrix(initial)
 	PrintMatrix(result)
+	if CompareMatrices(result, getExampleResult()) {
+		println("\nMatrices are IDENTICAL")
+	} else {
+		println("\nMatrices are DIFFERENT")
+	}
 
-	//if CompareMatrices(result, getExampleResult()) {
-	//	println("\nMatrices are IDENTICAL")
-	//	return
-	//}
-	//println("\nMatrices are DIFFERENT")
+	initial = getTaskInit()
+	result = SolveMatrix(initial)
+	PrintMatrix(result)
+
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -81,31 +86,60 @@ func getExampleResult() [][]Cell {
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-// Solve by pathfinding
+// Solve by pathfinding and intersection
 
 func SolveMatrix(matrix [][]Cell) [][]Cell {
-	//startingPoints := matrix[0]
-	//for i, _ := range startingPoints {
-	//	solution := makeWay(DuplicateMatrix(matrix), 2)
-	//	if solution != nil {
-	//		return solution
-	//	}
-	//}
-	solution := makeWay(DuplicateMatrix(matrix), 0, 4)
-	if solution != nil {
-		return solution
+	startingPointsDown := len(matrix[0])
+	startingPointsRight := len(matrix)
+
+	var solutionsDown [][][]Cell
+	var solutionsRight [][][]Cell
+
+	// make way down from the top
+	for i := 0; i < startingPointsDown; i++ {
+		solutionsDown = append(solutionsDown, makeWayDown(DuplicateMatrix(matrix), 0, i)...)
 	}
+
+	// make way from the left to the right
+	for j := 0; j < startingPointsRight; j++ {
+		solutionsRight = append(solutionsRight, makeWayRight(DuplicateMatrix(matrix), j, 0)...)
+	}
+
+	// combine both ways
+	// 1. find the intersection and filter by it
+	var filteredSolutions [][][]Cell
+	for _, solutionDown := range solutionsDown {
+		for _, solutionRight := range solutionsRight {
+			if checkIfHasIntersections(solutionDown, solutionRight) {
+
+				filteredSolutions = append(filteredSolutions, solutionDown, solutionRight)
+			}
+		}
+	}
+
+	// 2. combine the ways and check if it doesn't break the rules
+	for _, solutionDown := range solutionsDown {
+		for _, solutionRight := range solutionsRight {
+			combined := combineMatrixes(solutionDown, solutionRight)
+			if iterateMatrixAndCheckIfGood(combined) {
+				return combined
+			}
+		}
+	}
+
 	return nil
 }
 
-func makeWay(originMatrix [][]Cell, initRow, initColumn int) [][]Cell {
+var directions = [4][2]int{
+	{-1, 0}, // top
+	{1, 0},  // bottom
+	{0, -1}, // left
+	{0, 1},  // right
+}
+
+func makeWayDown(originMatrix [][]Cell, initRow, initColumn int) [][][]Cell {
 	// Initialize starting position
-	directions := [][]int{
-		{-1, 0}, // top
-		{1, 0},  // bottom
-		{0, -1}, // left
-		{0, 1},  // right
-	}
+
 	var solutions [][][]Cell
 
 	// Recursive function to explore cells
@@ -143,12 +177,62 @@ func makeWay(originMatrix [][]Cell, initRow, initColumn int) [][]Cell {
 				// go to the new position and discover path
 				discMatrix = explore(newMatrix, newRow, newColumn, 0)
 
-				if discMatrix[8][4].IsMarked == false {
-					PrintMatrix(discMatrix)
-				}
+				// go next direction
+			}
+		}
+		return discMatrix
+	}
 
-				// continue discovering path
-				discMatrix = explore(discMatrix, row, column, dirIndex+1)
+	explore(originMatrix, initRow, initColumn, 0)
+
+	var result [][][]Cell
+	for _, solution := range solutions {
+		if checkIfTouchesBottom(solution) {
+			result = append(result, solution)
+		}
+	}
+	return result
+}
+
+func makeWayRight(originMatrix [][]Cell, initRow, initColumn int) [][][]Cell {
+	// Initialize starting position
+
+	var solutions [][][]Cell
+
+	// Recursive function to explore cells
+	var explore func([][]Cell, int, int, int) [][]Cell
+	explore = func(matrix [][]Cell, row, column, startingDirIndex int) [][]Cell {
+		matrix[row][column].IsMarked = false
+
+		discMatrix := matrix
+
+		// Explore adjacent cells
+		for dirIndex := startingDirIndex; dirIndex < 4; dirIndex++ {
+			dir := directions[dirIndex]
+			// create new matrix to go the direction
+			newRow, newColumn := row+dir[0], column+dir[1]
+
+			// Check if the new position is within bounds else skip
+			if newRow < 0 || newRow >= len(matrix) || newColumn < 0 || newColumn >= len(matrix[0]) {
+				continue
+			}
+
+			// Check if the new position is not one of the previous positions
+			if matrix[newRow][newColumn].IsMarked == false {
+				continue
+			}
+
+			// Check if the new position has unique value to int the unmarked row
+			isUnique := checkIfUniqueWithinUnmarked(matrix, newRow, newColumn)
+
+			if isUnique {
+
+				// create new dimension where it goes to the new position
+				newMatrix := DuplicateMatrix(matrix)
+				solutions = append(solutions, newMatrix)
+
+				// go to the new position and discover path
+				discMatrix = explore(newMatrix, newRow, newColumn, 0)
 
 				// go next direction
 			}
@@ -158,16 +242,65 @@ func makeWay(originMatrix [][]Cell, initRow, initColumn int) [][]Cell {
 
 	explore(originMatrix, initRow, initColumn, 0)
 
+	var result [][][]Cell
 	for _, solution := range solutions {
-		if checkIfTouchesAllTheWalls(solution) {
-			return solution
+		if checkIfTouchesRight(solution) {
+			result = append(result, solution)
 		}
 	}
-	return nil
+	return result
 }
 
 /////////////////////////////////////////////////////////////////////////////////////
 // Check matrix by itself
+
+func checkIfHasIntersections(m1 [][]Cell, m2 [][]Cell) bool {
+	for i := 0; i < len(m1); i++ {
+		for j := 0; j < len(m1[0]); j++ {
+			if m1[i][j].IsMarked == false && m2[i][j].IsMarked == false {
+				return true
+			}
+		}
+	}
+	return false
+
+}
+
+func iterateMatrixAndCheckIfGood(matrix [][]Cell) bool {
+
+	// find first unmarked
+	var startCol int
+	for i := 0; i < len(matrix[0]); i++ {
+		if matrix[0][i].IsMarked == false {
+			startCol = i
+			break
+		}
+	}
+
+	isGood := true
+	visited := make(map[[2]int]bool) // Keep track of visited cells to avoid revisiting
+	var iterate func(int, int)
+	iterate = func(row int, col int) {
+		if row < 0 || col < 0 || row >= len(matrix) || col >= len(matrix[0]) || visited[[2]int{row, col}] || matrix[row][col].IsMarked == true {
+			return
+		}
+		// Mark the cell as visited
+		visited[[2]int{row, col}] = true
+		// Perform check operation
+		if checkIfUniqueWithinUnmarked(matrix, row, col) == false {
+			isGood = false
+			return
+		}
+		// Recursively call iterate for neighboring cells
+		iterate(row+1, col) // Right
+		iterate(row-1, col) // Left
+		iterate(row, col+1) // Down
+		iterate(row, col-1) // Up
+
+	}
+	iterate(0, startCol)
+	return isGood
+}
 
 func checkIfUniqueWithinUnmarked(matrix [][]Cell, rowIndex int, columnIndex int) bool {
 	value := matrix[rowIndex][columnIndex].Value
@@ -205,22 +338,35 @@ func checkSide(solution []Cell) bool {
 	return false
 }
 
-func checkIfTouchesAllTheWalls(solution [][]Cell) bool {
-	hasLeft, hasRight, hasBottom := false, false, false
+func checkIfTouchesBottom(solution [][]Cell) bool {
+	hasBottom := false
 
 	hasBottom = checkSide(solution[len(solution)-1])
 	if hasBottom == false {
 		return false
 	}
-	hasLeft = checkSide(getMatrixColumn(solution, 0))
-	if hasLeft == false {
-		return false
-	}
+	return true
+}
+
+func checkIfTouchesRight(solution [][]Cell) bool {
+	hasRight := false
 	hasRight = checkSide(getMatrixColumn(solution, len(solution[0])-1))
 	if hasRight == false {
 		return false
 	}
 	return true
+}
+
+func combineMatrixes(matrix1 [][]Cell, matrix2 [][]Cell) [][]Cell {
+	combined := DuplicateMatrix(matrix1)
+	for i := 0; i < len(matrix1); i++ {
+		for j := 0; j < len(matrix1[0]); j++ {
+			if matrix2[i][j].IsMarked == false {
+				combined[i][j].IsMarked = false
+			}
+		}
+	}
+	return combined
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -266,6 +412,5 @@ func CompareMatrices(matrix1, matrix2 [][]Cell) bool {
 			}
 		}
 	}
-
 	return true // Matrices are identical
 }
